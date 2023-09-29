61264d7a4df7e8922b5622b980ed1da63ac3c87f
Hao Duong <hao.duong11@gmail.com>
2023-09-29 23:55:38 +0900

    Add animation to about section

--- components/about.tsx
+++ components/about.tsx
@@ -1,10 +1,18 @@
 /* eslint-disable react/no-unescaped-entities */
+'use client';
+
 import React from 'react';
 import SectionHeading from './section-heading';
+import { motion } from 'framer-motion';
 
 export default function About() {
   return (
-    <section className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40">
+    <motion.section
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+    >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
         After graduating with a degree in{' '}
@@ -32,6 +40,6 @@ export default function About() {
         achieve N3 on the Japanese Language Proficiency Test by the end of this
         year.
       </p>
-    </section>
+    </motion.section>
   );
 }

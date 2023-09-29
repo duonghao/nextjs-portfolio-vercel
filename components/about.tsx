749d9f10786bb0715950c98d53ebaad83701ae2d
Hao Duong <hao.duong11@gmail.com>
2023-09-29 23:53:15 +0900

    Break out section headers into a component

--- components/about.tsx
+++ components/about.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
+import SectionHeading from './section-heading';
 
 export default function About() {
   return (
     <section className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40">
-      <h2 className="text-3xl font-medium capitalize mb-8">About me</h2>
+      <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
         After graduating with a degree in{' '}
         <span className="font-medium">Mechanical Engineering</span>, I decided

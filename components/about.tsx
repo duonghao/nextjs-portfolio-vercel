5f02728435adfa28aadc6a40764a2187df41f123
Hao Duong <hao.duong11@gmail.com>
2023-10-01 16:52:27 +0900

    Add projects section

--- components/about.tsx
+++ components/about.tsx
@@ -12,6 +12,7 @@ export default function About() {
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
+      id="about"
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">

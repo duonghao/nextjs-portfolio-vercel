470afa32b898c7dc5cc06bfd629443d1a0a661be
Hao Duong <hao.duong11@gmail.com>
2023-10-03 18:55:55 +0900

    Update CV

--- components/intro.tsx
+++ components/intro.tsx
@@ -85,7 +85,7 @@ export default function Intro() {
         </Link>
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV.pdf"
+          href="/HaoDuong_Resume.pdf"
           download
         >
           Download CV{' '}

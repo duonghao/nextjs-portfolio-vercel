8c3f69b9b6e44f9f5b9fd8cb71f38e7151583415
Hao Duong <hao.duong11@gmail.com>
2023-10-03 15:40:11 +0900

    Change positioning of theme switch

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -3,16 +3,19 @@
 import { useTheme } from '@/context/theme-context';
 import React from 'react';
 import { BsMoon, BsSun } from 'react-icons/bs';
+import { motion } from 'framer-motion';
 
 export default function ThemeSwitch() {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <button
-      className="flex items-center justify-center fixed bottom-5 right-5 bg-white h-[3rem] w-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
+    <motion.button
+      className="flex items-center fixed bottom-4 right-4 sm:static justify-center bg-white h-[3rem] w-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
       onClick={toggleTheme}
+      initial={{ opacity: 0, y: -100 }}
+      animate={{ opacity: 1, y: 0 }}
     >
       {theme === 'light' ? <BsSun></BsSun> : <BsMoon></BsMoon>}
-    </button>
+    </motion.button>
   );
 }

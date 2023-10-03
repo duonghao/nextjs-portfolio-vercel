06b8c4c2275d3b317606a5b8740db8487501a171
Hao Duong <hao.duong11@gmail.com>
2023-10-03 13:41:23 +0900

    Fix header section in view not changing for experience section

--- components/experience.tsx
+++ components/experience.tsx
@@ -12,7 +12,7 @@ import { useSectionInView } from '@/lib/hooks';
 import { useTheme } from '@/context/theme-context';
 
 export default function Experience() {
-  const { ref } = useSectionInView('experience');
+  const { ref } = useSectionInView('experience', 0.5);
   const { theme } = useTheme();
 
   return (
